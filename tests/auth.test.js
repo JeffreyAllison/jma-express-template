7f1900deb65b250a13b3d4f9c527ee24f3592748
JeffAllison <itsjallison@gmail.com>
2022-08-09 15:46:32 -0700

    added auth tests for duplicate email and signing in, wrote getbyEmail function and isValidPassword function in users model, created auth post route for singing in with getbyemail function and validpassword function, both tests passing

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,4 +1,6 @@
 const { setupDb, signUpUser } = require('./utils.js');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('/api/v1/auth', () => {
   beforeEach(setupDb);
@@ -14,4 +16,29 @@ describe('/api/v1/auth', () => {
     const { statusCode } = await agent.get('/api/v1/auth/verify');
     expect(statusCode).toBe(200);
   });
+});
+
+it('/signup with duplicate email', async () => {
+  await signUpUser();
+  const { res } = await signUpUser();
+  expect(res.statusCode).toBe(400);
+  expect(res.body).toEqual({
+    status: 400,
+    message: 'Email already exists',
+  });
+});
+
+it('/signin', async () => {
+  const { credentials } = await signUpUser();
+
+  const agent = request.agent(app);
+  const res = await agent.post('/api/v1/auth/signin').send(credentials);
+
+  expect(res.body).toEqual({
+    id: expect.any(String),
+    email: credentials.email,
+  });
+
+  const { statusCode } = await agent.get('/api/v1/auth/verify');
+  expect(statusCode).toBe(200);
 });
