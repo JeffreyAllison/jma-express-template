d8227bd245e545e614e64bdb0fd55b81645c9d0b
JeffAllison <itsjallison@gmail.com>
2022-08-09 16:46:39 -0700

    added test to get fruit by id, created getbyID function in fruit model, created get authorizedfruit route by ID, added params route to establish fruit id, added test to return 401 if user is not authenticated, both tests passing

--- tests/fruits.test.js
+++ tests/fruits.test.js
@@ -1,4 +1,6 @@
 const { setupDb, signUpUser } = require('./utils.js');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('/api/v1/fruits', () => {
   beforeEach(setupDb);
@@ -27,5 +29,24 @@ describe('/api/v1/fruits', () => {
     const resp2 = await agent2.get('/api/v1/fruits');
     expect(resp2.status).toEqual(200);
     expect(resp2.body).toEqual([user2Fruit]);
+  });
+
+  it('GET /:id should get a fruit', async () => {
+    const { agent } = await signUpUser();
+
+    const { body: fruit } = await agent.post('/api/v1/fruits').send({
+      name: 'apple',
+      color: 'red',
+    });
+
+    const { status, body: got } = await agent.get(`/api/v1/fruits/${fruit.id}`);
+
+    expect(status).toBe(200);
+    expect(got).toEqual(fruit);
+  });
+
+  it('GET / should return a 401 if user is not authenticated', async () => {
+    const { status } = await request(app).get('/api/v1/fruits');
+    expect(status).toEqual(401);
   });
 });
